2742d7910721894bb4e19a31285c6a770f9f7c5c
M Verbeek <maikel_verbeek@hotmail.com>
2016-09-12 15:10:08 +0200

    delete event.html - click function change to dynamic content

--- www/framework7/my-app.js
+++ www/framework7/my-app.js
@@ -22,7 +22,7 @@ myApp.onPageInit('index', function (page) {
 }).trigger();
 
 // Event page content and settings
-function createEventContent(image, name, startDate, endDate, description){ 
+function createEventContent(){ 
 	mainView.router.load({
 		content:'<div class="page no-toolbar navbar-fixed" data-page="event">'+
 				'	<div class="navbar inner-element uib_w_2" data-uib="framework7/header" data-ver="0">'+
@@ -39,13 +39,13 @@ function createEventContent(image, name, startDate, endDate, description){
 				'	         </div>'+
 				'	    </div>'+
 				'	</div>'+
-				'	<div class="page-content" style="background: url(' + image + ')" >'+
-				' 		<p>' + name + '</p>'+
-				' 		<p>' + startDate + '</p>'+
-				' 		<p>' + endDate + '</p>'+
+				'	<div class="page-content" style="background: url(' + /* image */ + ')" >'+
+				' 		<p>' + /* name */ + '</p>'+
+				' 		<p>' + /* startDate */ + '</p>'+
+				' 		<p>' + /* endDate */ + '</p>'+
 				'	    <div class="content-block cu-bottom cu-no-margin cu-no-padding">'+
 				'	    	<div class="row no-gutter">'+
-				'	            <div class="col-50 cu-extra-info">' + description + '</div>'+
+				'	            <div class="col-50 cu-extra-info">' + /* description */ + '</div>'+
 				'	            <div class="col-50 cu-maps"></div>'+
 				'	      	</div>'+
 				'	    </div>'+
@@ -56,8 +56,9 @@ function createEventContent(image, name, startDate, endDate, description){
 	return;	
 };
 
-$$('#event').click(function(e){
+$$(document.body).on('click', '#event',function(e){
 	e.preventDefault();
+	console.log('test');
 /*
 	// init var for function:	
 	var image = $(this).find('img').attr('src');
@@ -66,12 +67,8 @@ $$('#event').click(function(e){
 	var endDate = $(this).find('p.endDate').html();
 	var description = $(this).find('p.desc').html();
 */
-	
-	createEventContent(image, name, startDate, endDate, description);	
+	createEventContent();	
 });
-
-
-
 
 
 function getJsonData() {
@@ -80,7 +77,7 @@ function getJsonData() {
 		for(var i in json.events) {
 			var startDate = moment(json.events[i].timestamp_b * 1000).format("DD-MM-YYYY");
 			
-		    $$('ul.cu-events').append("<li class='media-item widget uib_w_11 d-margins' data-uib='framework7/media_item' data-ver='0'><a href='event.php'> <div class='item-content'><div class='item-media'><img src='" + json.events[i].images[0] + "' height='80'></div><div class='item-inner'><div class='item-title-row'><div class='item-title'>" + json.events[i].name + "</div><div class='item-after'>" + startDate + "</div></div><div class='item-subtitle'>" + json.events[i].location_details.name + "</div><div class='item-text'>" + json.events[i].description + "</div></div></div></a></li>");
+		    $$('ul.cu-events').append("<li class='media-item widget uib_w_11 d-margins' data-uib='framework7/media_item' data-ver='0'><a href='#' id='event'> <div class='item-content'><div class='item-media'><img src='" + json.events[i].images[0] + "' height='80'></div><div class='item-inner'><div class='item-title-row'><div class='item-title'>" + json.events[i].name + "</div><div class='item-after'>" + startDate + "</div></div><div class='item-subtitle'>" + json.events[i].location_details.name + "</div><div class='item-text'>" + json.events[i].description + "</div></div></div></a></li>");
 		}
 	});	
 }
